6b3848d73f8a6f07e9d04d753024dddb8ff4092c
nicolasconil <conilnicolas1@gmail.com>
2025-08-02 13:38:27 -0300

    fix: csrf.routes.js fixed

--- routes/csrf.routes.js
+++ routes/csrf.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { csrfProtection, addCsrfToken } from "../middleware/csrf.middleware.js";
+import { addCsrfToken } from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 
-router.get('/csrf-token', csrfProtection, addCsrfToken, (req, res) => {
+router.get('/csrf-token', addCsrfToken, (req, res) => {
     res.json({ csrfToken: res.locals.csrfToken });
 });
 
