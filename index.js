22631891992437ff8bc4d5882e7ed938d696d382
nicolasconil <conilnicolas1@gmail.com>
2025-08-02 16:42:27 -0300

    fix: index.js fixed

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-XSRF-TOKEN', 'XSRF-TOKEN'],
 }));
 
 app.use(cookieParser());
